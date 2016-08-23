c38f3133c2eb8eb3cfda239ea7b226bac07a96e1
Marcin Kelar <marcin.kelar@gmail.com>
2016-08-23 09:24:53 +0200

    Miscellaneous string functions - header

---
+++ include/string.h
@@ -0,0 +1,11 @@
+/*******************************************************************
+WebSocket Protocol Implementation
+Author: Marcin Kelar ( marcin.kelar@gmail.com )
+*******************************************************************/
+#ifndef STRING_H
+#define STRING_H
+
+int		xdigit( char digit );
+int		xstr2str( char *buf, unsigned bufsize, const char *in );
+
+#endif
